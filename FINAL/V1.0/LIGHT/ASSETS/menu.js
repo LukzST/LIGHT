b85dcf1131ea4c5a5149f693446658bd00bf4d3d
LukzST <lucaseduarte6@gmail.com>
2026-01-19 13:17:55 -0300

    V1.0-FINAL

--- FINAL/V1.0/LIGHT/ASSETS/menu.js
+++ FINAL/V1.0/LIGHT/ASSETS/menu.js
@@ -1,4 +1,4 @@
-const CURRENT_VERSION = "V1.01";
+const CURRENT_VERSION = "V1.0";
 const blessed = require('blessed');
 const os = require('os');
 const { spawn } = require('child_process');
@@ -905,7 +905,7 @@ const copyrightBOX1 = blessed.box({
  right: '0',
  width: 'shrink',
  height: 1,
- content: 'V1.01',
+ content: 'V1.0',
  tags: true,
  style: {
  fg: color,
