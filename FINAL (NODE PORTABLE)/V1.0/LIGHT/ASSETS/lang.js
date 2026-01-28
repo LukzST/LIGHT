8bf3294a3febaae6f19937bc028906cd0b635bcc
LukzST <lucaseduarte6@gmail.com>
2026-01-28 00:51:36 -0300

    V1.0-FINAL

---
+++ FINAL (NODE PORTABLE)/V1.0/LIGHT/ASSETS/lang.js
@@ -0,0 +1,17 @@
+// locales.js
+module.exports = {
+    'PT': {
+        'START MISSION': 'INICIAR MISSÃO',
+        'SETTINGS': 'CONFIGURAÇÕES',
+        'EXIT': 'SAIR',
+        'ACHIEVEMENTS': 'CONQUISTAS',
+        'SYSTEM INFO': 'INFO DO SISTEMA',
+        'CHECKPOINTS': 'PONTOS DE CONTROLE',
+        'ERASE DATA': 'APAGAR DADOS',
+        'CREDITS': 'CRÉDITOS',
+        'SUPPORT': 'SUPORTE',
+        'SELECT AN OPTION USING ARROW KEYS AND PRESS ENTER': 'SELECIONE UMA OPÇÃO COM AS SETAS E APERTE ENTER',
+        // Adicione as frases da narrativa aqui exatamente como estão no main.js
+        '[NARRATOR]: You wake up in your bed...': '[NARRADOR]: Você acorda em sua cama...',
+    }
+};
